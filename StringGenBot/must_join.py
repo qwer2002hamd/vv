a2de13070eb8ffa4864a2d4b7c711844eec3f9a6
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-06-22 14:11:11 +0300

    must_join.py

--- StringGenBot/must_join.py
+++ StringGenBot/must_join.py
@@ -1,4 +1,4 @@
-from env import MUST_JOIN
+from config import MUST_JOIN
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
